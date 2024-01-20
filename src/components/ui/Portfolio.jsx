9c45a849612102cabb26fa1fb73fe2bbce8f4f2c
Kyle Irbing <tobiasrf22@gmail.com>
2024-01-20 00:25:13 -0300

    updated portfolio
    Portfolio update. Now the techs are more beatiful and are better than before.
    
    Also I fix te border problem with the cards, between portfolio, experience and education.

--- src/components/ui/Portfolio.jsx
+++ src/components/ui/Portfolio.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { FaExternalLinkAlt } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { FaSass } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+import { SiGulp } from "react-icons/si";
+import { FaPython } from "react-icons/fa";
 
 
 
@@ -8,17 +13,54 @@ export const Portfolio = () => {
     <section id="portfolio" className="portfolio contenedor2">
       <h2>Portfolio</h2>
       <div className='cards'>
-        <div className='card'>
+        <div className='cardP'>
           <h3>Weather Page</h3>
-          <h4>React, Api, Sass</h4>
+          <div className='technologies'>
+            <div className='tech react'>
+              <FaReact size={20} />
+              <h4>React</h4>
+            </div>
+            <div className='tech sass'>
+              <FaSass size={20} />
+              <h4>Sass</h4>
+            </div>
+            <div className='tech javascript'>
+              <IoLogoJavascript size={20} />
+              <h4>Javascript</h4>
+            </div>
+          </div>
           <p>Build from scratch, it use openweatherAPI</p>
           <a target="_blank" href='https://weather-react-app-lemon.vercel.app/'>Live demo <FaExternalLinkAlt size={10} /></a>
         </div>
-        <div className='card'>
+        <div className='cardP'>
           <h3>rockedmfesival</h3>
-          <h4>Javascript, Gulp, Sass</h4>
+          <div className='technologies'>
+            <div className='tech javascript'>
+              <IoLogoJavascript size={20} />
+              <h4>Javascript</h4>
+            </div>
+            <div className='tech gulp'>
+              <SiGulp size={20} />
+              <h4>Gulp</h4>
+            </div>
+            <div className='tech sass'>
+              <FaSass size={20} />
+              <h4>Sass</h4>
+            </div>
+          </div>
           <p>Gulp, Sass, and JavaScript for automation, styling, and functionality.</p>
           <a target="_blank" href='https://rockedmfesival.netlify.app/'>Live demo <FaExternalLinkAlt size={10} /></a>
+        </div>
+        <div className='cardP'>
+          <h3>Python Api</h3>
+          <div className='technologies'>
+            <div className='tech python'>
+              <FaPython size={20} />
+              <h4>Python</h4>
+            </div>
+          </div>
+          <p>API developed by me for a personal project, uses tokens for posts and returns a JSON of products.</p>
+          <a target="_blank" href='https://weather-react-app-lemon.vercel.app/'>Live demo <FaExternalLinkAlt size={10} /></a>
         </div>
       </div>
     </section>
