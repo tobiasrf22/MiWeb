a2b4f23a316f43b94738b499358ecf85e0d5ed8c
Kyle Irbing <31706313+tobiasrf22@users.noreply.github.com>
2024-01-23 17:16:08 -0300

    fix api link portfolio

--- src/components/ui/Portfolio.jsx
+++ src/components/ui/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
 import { FaReact } from "react-icons/fa";
 import { FaSass } from "react-icons/fa";
@@ -6,68 +6,79 @@ import { IoLogoJavascript } from "react-icons/io";
 import { SiGulp } from "react-icons/si";
 import { FaPython } from "react-icons/fa";
 import { FaNodeJs } from "react-icons/fa";
-
-
+import { FaGithub } from "react-icons/fa";
 
 export const Portfolio = () => {
   return (
     <section id="portfolio" className="portfolio contenedor2">
       <h2>Portfolio</h2>
-      <div className='cards'>
-        <div className='cardP'>
+      <div className="cards">
+        <div className="cardP">
           <h3>Weather Page</h3>
-          <div className='technologies'>
-            <div className='tech react'>
+          <div className="technologies">
+            <div className="tech react">
               <FaReact size={20} />
               <h4>React</h4>
             </div>
-            <div className='tech sass'>
+            <div className="tech sass">
               <FaSass size={20} />
               <h4>Sass</h4>
             </div>
-            <div className='tech javascript'>
+            <div className="tech javascript">
               <IoLogoJavascript size={20} />
               <h4>Javascript</h4>
             </div>
-            <div className='tech nodejs'>
-              <FaNodeJs  size={20} />
+            <div className="tech nodejs">
+              <FaNodeJs size={20} />
               <h4>NodeJS</h4>
             </div>
           </div>
           <p>Build from scratch, it use openweatherAPI</p>
-          <a target="_blank" href='https://weather-react-app-lemon.vercel.app/'>Live demo <FaExternalLinkAlt size={10} /></a>
+          <a target="_blank" href="https://weather-react-app-lemon.vercel.app/">
+            Live demo <FaExternalLinkAlt size={10} />
+          </a>
         </div>
-        <div className='cardP'>
+        <div className="cardP">
           <h3>rockedmfesival</h3>
-          <div className='technologies'>
-            <div className='tech javascript'>
+          <div className="technologies">
+            <div className="tech javascript">
               <IoLogoJavascript size={20} />
               <h4>Javascript</h4>
             </div>
-            <div className='tech gulp'>
+            <div className="tech gulp">
               <SiGulp size={20} />
               <h4>Gulp</h4>
             </div>
-            <div className='tech sass'>
+            <div className="tech sass">
               <FaSass size={20} />
               <h4>Sass</h4>
             </div>
           </div>
-          <p>Gulp, Sass, and JavaScript for automation, styling, and functionality.</p>
-          <a target="_blank" href='https://rockedmfesival.netlify.app/'>Live demo <FaExternalLinkAlt size={10} /></a>
+          <p>
+            Gulp, Sass, and JavaScript for automation, styling, and
+            functionality.
+          </p>
+          <a target="_blank" href="https://rockedmfesival.netlify.app/">
+            Live demo <FaExternalLinkAlt size={10} />
+          </a>
         </div>
-        <div className='cardP'>
+        <div className="cardP">
           <h3>Python Api</h3>
-          <div className='technologies'>
-            <div className='tech python'>
+          <div className="technologies">
+            <div className="tech python">
               <FaPython size={20} />
               <h4>Python</h4>
             </div>
           </div>
-          <p>API developed by me for a personal project, uses tokens for posts and returns a JSON of products.</p>
-          <a target="_blank" href='https://weather-react-app-lemon.vercel.app/'>Live demo <FaExternalLinkAlt size={10} /></a>
+          <p>
+            API developed by me for a personal project, uses tokens for post,
+            delete and returns a JSON.
+          </p>
+          <a target="_blank" href="https://github.com/tobiasrf22/myweb-api-">
+            Github <FaGithub size={20} />
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
